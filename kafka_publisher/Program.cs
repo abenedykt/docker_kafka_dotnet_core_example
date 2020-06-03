ce0dda0b2d2f3cecf82c1769c71b99d049adb984
Arkadiusz Benedykt <ARKADIUSZ.BENEDYKT@pb.com>
2020-06-03 15:23:09 +0200

    * docker compose with bitnami kafka * consumer: exmaple that publish to the docerized kafka * producer: example that consumes messages published to the docerized   kafka

---
+++ kafka_publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+        // If serializers are not specified, default serializers from
+        // `Confluent.Kafka.Serializers` will be automatically used where
+        // available. Note: by default strings are encoded as UTF8.
+        using (var p = new ProducerBuilder<Null, string>(config).Build())
+        {
+            try
+            {
+                for(var i = 0; i < 1000; i++){
+                    var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value="test" });
+                    await Task.Delay(100);
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+            }
+            catch (ProduceException<Null, string> e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            }
+        }
+    }
+}
